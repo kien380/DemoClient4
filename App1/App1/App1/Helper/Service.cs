b993a1749c6a92d7621c48c4bb88d64f398de01a
Ken Huynh <kienhht@ilandapp.com>
2017-05-03 00:59:07 +0700

    abc

--- App1/App1/App1/Helper/Service.cs
+++ App1/App1/App1/Helper/Service.cs
@@ -1,53 +1,102 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace App1.Helper
+namespace App1
 {
     public class Service
     {
-        //private async void LoadDataFromServer(List<ListItemsModal> itemSource)
-        //{
-        //    LoadingIcon.IsVisible = true;
-        //    LoadingIcon.IsRunning = true;
+        public Service()
+        {
 
-        //    var client = new HttpClient(); //thiết lập HttpClient() để truyền dữ liệu bằng http
-        //    string url = "http://ilandapp.com/demoproject/projectlistitem/listitem.php?pass=12569"; //link chứa chuỗi Json.
-        //    var uri = new Uri(url); //Getlink
-        //    var response = await client.GetAsync(uri); //Kết nối với link giao thức Get
-        //    if (response.IsSuccessStatusCode) //Kiểm tra kết nối 
-        //    {
-        //        var contentGet = await response.Content.ReadAsStringAsync();
-        //        //Đọc dữ liệu kết nối đưa vào biến contentGet
-        //        var items = JsonConvert.DeserializeObject<List<ListItemsModal>>(contentGet);
-        //        /*Dùng JsonConvert để đưa dữ liệu từ json (trong biến contentGet) thành 1 List với kiểu dữ liệu là data*/
+        }
 
-        //        LoadingIcon.IsRunning = false;
-        //        LoadingIcon.IsVisible = false;
+        public async Task<List<Studio>> GetStudios ()
+        {
+            var studios = new List<Studio>();
+            var client = new HttpClient(); //thiết lập HttpClient() để truyền dữ liệu bằng http
+            string url = "http://iseteam.com/lichchieuphim/index.php/api/getstudio"; //link chứa chuỗi Json.
+            var uri = new Uri(url); //Getlink
+            try
+            {
+                var response = await client.GetAsync(uri); //Kết nối với link giao thức Get
+                if (response.IsSuccessStatusCode) //Kiểm tra kết nối 
+                {
+                    //Đọc dữ liệu kết nối đưa vào biến contentGet
+                    var contentGet = await response.Content.ReadAsStringAsync();
+                    //Dùng JsonConvert để đưa dữ liệu từ json (trong biến contentGet) thành 1 List với kiểu dữ liệu là data
+                    var items = JsonConvert.DeserializeObject<List<Studio>>(contentGet);
+                    if(items != null)
+                    {
+                        foreach(var item in items)
+                        {
+                            studios.Add(item);
+                        }
+                    }
+                    }
 
+                }
+            catch { }
+            return studios;
+        }
 
-        //        foreach (ListItemsModal item in items)
-        //        {
+        public async Task<List<Cinema>> GetCinemas(string studioName)
+        {
+            var cinemas = new List<Cinema>();
+            var client = new HttpClient(); //thiết lập HttpClient() để truyền dữ liệu bằng http
+            string url = "http://iseteam.com/lichchieuphim/index.php/api/getcinema/cinema/" + studioName.ToLower(); //link chứa chuỗi Json.
+            var uri = new Uri(url); //Getlink
+            try
+            {
+                var response = await client.GetAsync(uri); //Kết nối với link giao thức Get
+                if (response.IsSuccessStatusCode) //Kiểm tra kết nối 
+                {
+                    //Đọc dữ liệu kết nối đưa vào biến contentGet
+                    var contentGet = await response.Content.ReadAsStringAsync();
+                    //Dùng JsonConvert để đưa dữ liệu từ json (trong biến contentGet) thành 1 List với kiểu dữ liệu là data
+                    var items = JsonConvert.DeserializeObject<List<Cinema>>(contentGet);
+                    if (items != null)
+                    {
+                        foreach (var item in items)
+                        {
+                            cinemas.Add(item);
+                        }
+                    }
+                }
+            }
+            catch { }
+            return cinemas;
+        }
 
-        //            itemSource.Add(new ListItemsModal
-        //            {
-        //                Name = item.Name,
-        //                Price = item.Price,
-        //                PictureURI = new UriImageSource
-        //                {
-        //                    Uri = new Uri(item.PictureUrl),
-        //                    CachingEnabled = true,
-        //                    CacheValidity = new TimeSpan(5, 0, 0, 0)
-        //                },
-        //                Detail = item.Detail,
-        //                CommandEvent = new Command(Ontap)
-        //            });
-
-        //            //await DisplayAlert("", item.PictureUrl, "OK");
-        //        }
-        //    }
-
-        //}
-
+        public async Task<List<LichChieu>> GetLichChieu(string urlRap)
+        {
+            var dsLichChieu = new List<LichChieu>();
+            var client = new HttpClient(); //thiết lập HttpClient() để truyền dữ liệu bằng http
+            string url = "http://iseteam.com/lichchieuphim/index.php/api/getfilm/rap/" + urlRap; //link chứa chuỗi Json.
+            var uri = new Uri(url); //Getlink
+            try
+            {
+                var response = await client.GetAsync(uri); //Kết nối với link giao thức Get
+                if (response.IsSuccessStatusCode) //Kiểm tra kết nối 
+                {
+                    //Đọc dữ liệu kết nối đưa vào biến contentGet
+                    var contentGet = await response.Content.ReadAsStringAsync();
+                    //Dùng JsonConvert để đưa dữ liệu từ json (trong biến contentGet) thành 1 List với kiểu dữ liệu là data
+                    var items = JsonConvert.DeserializeObject<List<LichChieu>>(contentGet);
+                    if (items != null)
+                    {
+                        foreach (var item in items)
+                        {
+                            dsLichChieu.Add(item);
+                        }
+                    }
+                }
+            }
+            catch { }
+            return dsLichChieu;
+        }
     }
 }
